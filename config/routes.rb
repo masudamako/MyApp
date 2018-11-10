7409d5be80fee3a4c3fa1dc8d5005bd2b6ac0dea
masudamako <43661063+masudamako@users.noreply.github.com>
2018-11-11 01:03:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'messagea#index'
-  resources :messages
+  root 'dialies#index'
+  resources :dialies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 root
